85e89741cd7936768b921222ae8328b887d3f2c8
Dawn Sheedy <sheedy.dawn@gmail.com>
2020-08-02 20:21:09 -0400

    added config file

---
+++ config.js
@@ -0,0 +1,8 @@
+export default {
+    obsSettings: { //Address and port of PC running obs with obs-websockets
+        address: "localhost",
+        port: "4444"
+    },
+    ccServer: "http://cc.dawnsheedy.com",//URL for cc server
+    ccToken: "none" //token for cc user on the server. access level should be that of a regular user
+}
--- index.js
+++ index.js
@@ -1,19 +1,17 @@
+const options = require('./config');
 const OBSWebSocket = require('obs-websocket-js');
-const socketio = require('socket.io-client')('http://cc.dawnsheedy.com');
+const socketio = require('socket.io-client')(options.ccServer);
 const obs = new OBSWebSocket();
 
 let lastCaption = {speaker: {id: "none"}};
 
-obs.connect({
-    address: '192.168.1.48:4444',
-    password: 'password'
-}).then(() => {
+obs.connect(options.obsSettings).then(() => {
     console.log("Connected to OBS");
 })
 
 socketio.on('connect', function () {
     console.log("Connected to CC Service")
-    socketio.emit('auth', {token: '7ahvhad7fbHBhabnbasdf7HCh'});
+    socketio.emit('auth', {token: options.ccToken});
 })
 
 socketio.on('user-assignment', (data) => {
